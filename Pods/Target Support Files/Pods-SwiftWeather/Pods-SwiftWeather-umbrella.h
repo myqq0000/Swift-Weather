999425aba64d64d5408caa2cc1f851933ad564da
杨逸飞 <Vincent@yangyifeideMacBook-Pro.local>
2016-06-04 00:19:28 +0800

    first commit

---
+++ Pods/Target Support Files/Pods-SwiftWeather/Pods-SwiftWeather-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SwiftWeatherVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SwiftWeatherVersionString[];
+
---
+++ Pods/Target Support Files/Pods-SwiftWeatherTests/Pods-SwiftWeatherTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftWeatherTests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftWeatherTests
+@end
